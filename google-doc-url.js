e2682eca7f8265220aac0ae3d831db9c7fd36709
Jack Dougherty <jack.dougherty@trincoll.edu>
2020-10-23 14:31:01 -0400

    updated to v1.2

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/13bFHsXJtADIkUCGgjpO76RjrV0OAu62hzK-J3bXEVsk/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
